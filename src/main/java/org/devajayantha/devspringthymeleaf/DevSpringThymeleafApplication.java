bbf4cf9b0c48bbce602369489306857617790d2d
Devajayantha <devajayantha888@gmail.com>
2024-05-16 22:11:26 +0800

    first commit

---
+++ src/main/java/org/devajayantha/devspringthymeleaf/DevSpringThymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.devajayantha.devspringthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpringThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevSpringThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devajayantha/devspringthymeleaf/DevSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devajayantha.devspringthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
